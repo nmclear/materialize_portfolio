48b96ab230778d9a1230e6192af74b9c7809dde5
Nick Clear <nickclear22@gmail.com>
2018-05-24 22:38:54 -0700

    fixed responsiveness of home social icons. added rest of click navbar click listeners.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
     //Show side Nav
     $('.sidenav').sidenav();
 
-    
+//========================================================================================================================
+// ON PAGE LOAD CALLS
+//========================================================================================================================
 //on page load, show HomePage, hide all others
     $('#containerHome').show();
     $('#containerAbout').hide();
@@ -12,6 +14,9 @@ $(document).ready(function(){
     $('#containerHobbies').hide();
     $('#containerContact').hide();
 
+//========================================================================================================================
+// NAVBAR CLICK LISTENERS
+//========================================================================================================================
     $('#navHome').on('click', function(){
         $('#containerHome').show();
         $('#containerAbout').hide();
@@ -38,6 +43,37 @@ $(document).ready(function(){
         $('#containerHobbies').hide();
         $('#containerContact').hide();
     });
+
+    $('#navProjects').on('click', function(){
+        $('#containerHome').hide();
+        $('#containerAbout').hide();
+        $('#containerEducation').hide();
+        $('#containerProjects').show();
+        $('#containerHobbies').hide();
+        $('#containerContact').hide();
+    });
+
+    $('#navHobbies').on('click', function(){
+        $('#containerHome').hide();
+        $('#containerAbout').hide();
+        $('#containerEducation').hide();
+        $('#containerProjects').hide();
+        $('#containerHobbies').show();
+        $('#containerContact').hide();
+    });
+
+    $('#navContact').on('click', function(){
+        $('#containerHome').hide();
+        $('#containerAbout').hide();
+        $('#containerEducation').hide();
+        $('#containerProjects').hide();
+        $('#containerHobbies').hide();
+        $('#containerContact').show();
+    });
+
+//========================================================================================================================
+//
+//========================================================================================================================
 
 
 
