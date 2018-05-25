9c482e7ba1b6e1d297319843639738bb5b91c8ce
Nick Clear <nickclear22@gmail.com>
2018-05-24 22:16:20 -0700

    added show/hide features for navHome, etc pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,40 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
 
     
+//on page load, show HomePage, hide all others
+    $('#containerHome').show();
+    $('#containerAbout').hide();
+    $('#containerEducation').hide();
+    $('#containerProjects').hide();
+    $('#containerHobbies').hide();
+    $('#containerContact').hide();
 
+    $('#navHome').on('click', function(){
+        $('#containerHome').show();
+        $('#containerAbout').hide();
+        $('#containerEducation').hide();
+        $('#containerProjects').hide();
+        $('#containerHobbies').hide();
+        $('#containerContact').hide();
+    });
 
+    $('#navAbout').on('click', function(){
+        $('#containerHome').hide();
+        $('#containerAbout').show();
+        $('#containerEducation').hide();
+        $('#containerProjects').hide();
+        $('#containerHobbies').hide();
+        $('#containerContact').hide();
+    });
+
+    $('#navEducation').on('click', function(){
+        $('#containerHome').hide();
+        $('#containerAbout').hide();
+        $('#containerEducation').show();
+        $('#containerProjects').hide();
+        $('#containerHobbies').hide();
+        $('#containerContact').hide();
+    });
 
 
 
